5616dab5cbf436fe0b78321fdbb44bcadba9b882
Walter Sobral Andrade <wsacin@gmail.com>
2025-04-23 13:23:12 -0400

    Changing to github plugin.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+--return {}
+return {
+  {
+    'wsacin/nvim-pyenv-loader',
+    opts = {},
+  },
+}
