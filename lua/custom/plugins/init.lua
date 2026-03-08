0a5938514170dbe8d687a7ae8f1e7e462fcaf1e6
Walter Sobral Andrade <walter.andrade@connectngo.com>
2026-03-08 13:36:15 -0400

    Adding session manager.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -208,4 +208,25 @@ return {
       }
     end,
   },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    keys = {
+      -- Will use Telescope if installed or a vim.ui.select picker otherwise
+      { '<leader>wr', '<cmd>AutoSession search<CR>', desc = 'Session search' },
+      { '<leader>ws', '<cmd>AutoSession save<CR>', desc = 'Save session' },
+      { '<leader>wa', '<cmd>AutoSession toggle<CR>', desc = 'Toggle autosave' },
+    },
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    },
+  },
+  {
+    'sindrets/diffview.nvim',
+  },
 }
